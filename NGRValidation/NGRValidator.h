91274909a2d0f0757cd8d83c0242f5b90d87a92d
Dmitry <sdkdimon@gmail.com>
2023-10-02 21:05:13 +0300

    Added NGRValidator delegate nullability

--- NGRValidation/NGRValidator.h
+++ NGRValidation/NGRValidator.h
@@ -34,7 +34,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @return result of validation. YES if validation will pass, NO otherwise.
  */
-+ (BOOL)validateModel:(NSObject *)model error:(NSError **)error delegate:(id<NGRMessaging>)delegate rules:(NGRRules)rules;
++ (BOOL)validateModel:(NSObject *)model error:(NSError **)error delegate:(nullable id<NGRMessaging>)delegate rules:(NGRRules)rules;
 
 /**
  *  Validates model with given rules. Returns YES when validation succeeded, NO otherwise.
@@ -47,7 +47,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @return result of validation. YES if validation will pass, NO otherwise.
  */
-+ (BOOL)validateModel:(NSObject *)model error:(NSError **)error scenario:(nullable NSString *)scenario delegate:(id<NGRMessaging>)delegate rules:(NGRRules)rules;
++ (BOOL)validateModel:(NSObject *)model error:(NSError **)error scenario:(nullable NSString *)scenario delegate:(nullable id<NGRMessaging>)delegate rules:(NGRRules)rules;
 
 /**
  *  Validates model with given rules. Returns empty array when validation succeeded. If not, array will contains errors.
@@ -58,7 +58,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @return Nil if validation will pass, otherwise an array populated with validation errors.
  */
-+ (nullable NSArray *)validateModel:(NSObject *)model delegate:(id<NGRMessaging>)delegate rules:(NGRRules)rules;
++ (nullable NSArray *)validateModel:(NSObject *)model delegate:(nullable id<NGRMessaging>)delegate rules:(NGRRules)rules;
 
 /**
  *  Validates model with given rules and scenario. Returns empty array when validation succeeded. If not, array will contains errors.
@@ -70,7 +70,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @return Nil if validation will pass, otherwise an array populated with validation errors.
  */
-+ (nullable NSArray *)validateModel:(NSObject *)model scenario:(nullable NSString *)scenario delegate:(id<NGRMessaging>)delegate rules:(NGRRules)rules;
++ (nullable NSArray *)validateModel:(NSObject *)model scenario:(nullable NSString *)scenario delegate:(nullable id<NGRMessaging>)delegate rules:(NGRRules)rules;
 
 @end
 
